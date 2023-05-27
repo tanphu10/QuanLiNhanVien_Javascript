bfca91eb3249fbd37c2438cf52da3f5aef8c6109
Tan Phu <phantanphu10@gmail.con>
2023-05-28 01:19:06 +0700

    update find out Student

--- controller/index.js
+++ controller/index.js
@@ -1,12 +1,13 @@
 var arr = [];
+var arrXepLoai = [];
 getStorage();
-renderGiaoDien();
-function renderGiaoDien() {
+renderGiaoDien(arr);
+function renderGiaoDien(arr) {
   var content = "";
   for (i = 0; i < arr.length; i++) {
-    var nhanVienUuTu = new NhanVien;
+    var nhanVienUuTu = new NhanVien();
     var nhanVienitem = arr[i];
-    Object.assign(nhanVienUuTu,nhanVienitem)
+    Object.assign(nhanVienUuTu, nhanVienitem);
     content += `<tr>
    <td>${nhanVienUuTu.taiKhoan}</td>
    <td>${nhanVienUuTu.hoTen}</td>
@@ -35,13 +36,13 @@ function renderGiaoDien() {
 // thêm nhân viên vào mảng
 document.getElementById("btnThemNV").onclick = function () {
   var nhanVienUuTu = layGiaTriInput();
-  if(nhanVienUuTu){
-  arr.push(nhanVienUuTu);
-  console.log(nhanVienUuTu);
-  saveStorage(arr);
-  renderGiaoDien();
-  ganGiaTriVaoInput("", "", "", "", "", "", "", "", "");
-}
+  if (nhanVienUuTu) {
+    arr.push(nhanVienUuTu);
+    console.log(nhanVienUuTu);
+    saveStorage(arr);
+    renderGiaoDien(arr);
+    ganGiaTriVaoInput("", "", "", "", "", "", "", "");
+  }
 };
 
 // // để xóa được nhân viên
@@ -57,7 +58,7 @@ chạy vòng lặp for
   if (index != -1) {
     arr.splice(index, 1);
   }
-  renderGiaoDien();
+  renderGiaoDien(arr);
 }
 
 // chỉnh sửa thông tin nhân viên
@@ -79,25 +80,66 @@ function editNhanVien(taiKhoan) {
     nhanVien.gioLamTrongThang
   );
   //   renderGiaoDien();
-  // document.getElementById("tknv").readOnly = true;
-  document.getElementById("email").readOnly = true;
+  document.getElementById("tknv").readOnly = true;
+  // document.getElementById("email").readOnly = true;
 }
 
-
-
-// update thông tin nhân viên 
+// update thông tin nhân viên
 document.getElementById("btnCapNhat").onclick = function (taiKhoan) {
-
   /*
 trình tự làm bài : 
 chạy vòng lặp tìm được index trong mảng , sau đó gán index = giá trị đã cập nhập
 */
   //   var nhanVienUpdate = ;
-  var updateNhanVien= layGiaTriInput();
+  var updateNhanVien = layGiaTriInput();
   var index = timNhanVien(updateNhanVien.taiKhoan);
   arr[index] = updateNhanVien;
   console.log(arr[index]);
-  renderGiaoDien();
-  ganGiaTriChoInput("", "", "", "", "", "", "", "", "");
+  renderGiaoDien(arr);
+  ganGiaTriChoInput("", "", "", "", "", "", "", "");
   // sau khi tìm ra được vị trí index thì làm ntn
 };
+
+// tìm kiếm giá trị bên trong mảng array và lọc ra xuất sắc hay giỏi hay cà rốt sẽ xuất hiện
+
+document.getElementById("btnTimNV").onclick = function () {
+  var loaiNhanVien = document.getElementById("searchName").value;
+  arrXepLoai=[];
+  for (var i = 0; i < arr.length; i++) {
+    var nhanVienOne = new NhanVien();
+    var nhanVienitem = arr[i];
+    Object.assign(nhanVienOne, nhanVienitem);
+    if (nhanVienOne.xepLoai() == loaiNhanVien) {
+      arrXepLoai.push(nhanVienOne);
+      // console.log('second');
+    } else if (loaiNhanVien == "") {
+      // renderGiaoDien(arr);
+      console.log('first')
+    }
+  }
+  // console.log(arrXepLoai)
+      renderGiaoDien(arrXepLoai);
+};
+// var soN = new NhanVien();
+// var arrNew=arr;
+
+// var index =arr.map(e => e.(`${soN.xepLoai}`).indexOf(`${loaiNhanVien}`));
+// arr[index] = layGiaTriInput();
+// console.log(arr)
+// renderGiaoDien();
+
+/*
+  input: nhập dữ liệu tìm kiếm xuất sắt, khá, giỏi kém
+
+  
+  
+  output : render cho người dùng hiển thị xuất sắc , khá giỏi , kém tương ứng với tim kím
+  */
+
+function printDiv(divName) {
+  var printContents = document.getElementById(divName).innerHTML;
+  var originalContents = document.body.innerHTML;
+  document.body.innerHTML = printContents;
+  window.print();
+  document.body.innerHTML = originalContents;
+}
